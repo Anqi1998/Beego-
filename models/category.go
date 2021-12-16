75b4f8a94441a355db0b2c159479cb55df100e73
Anqi1998 <32508260+Anqi1998@users.noreply.github.com>
2021-12-16 13:25:09 +0800

    Add files via upload

---
+++ models/category.go
@@ -0,0 +1,45 @@
+package models
+
+import (
+	"errors"
+	"strings"
+
+	"github.com/astaxie/beego/orm"
+)
+
+type Category struct {
+	Id     int
+	Pid    int    //分类id
+	Title  string `orm:"size(30);unique"`
+	Intro  string //介绍
+	Icon   string
+	Cnt    int  //统计分类下图书
+	Sort   int  //排序
+	Status bool //状态，true 显示
+}
+
+func (m *Category) TableName() string {
+	return TNCategory()
+}
+
+func (m *Category) GetCates(pid int, status int) (cates []Category, err error) {
+	qs := orm.NewOrm().QueryTable(TNCategory())
+	if pid > -1 {
+		qs = qs.Filter("pid", pid)
+	}
+
+	if status == 0 || status == 1 {
+		qs = qs.Filter("status", status)
+	}
+	_, err = qs.OrderBy("-status", "sort", "title").All(&cates)
+	return
+}
+
+//查询分类
+func (m *Category) Find(id int) (cate Category) {
+	cate.Id = id
+	orm.NewOrm().Read(&cate)
+	return cate
+}
+
+
